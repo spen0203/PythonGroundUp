88e043a82b69485028d7816f9f0af6a114293f97
spen0203 <56701568+spen0203@users.noreply.github.com>
2020-06-13 21:20:05 -0400

    fixed missing file

--- Python WebData/XML.py
+++ Python WebData/XML.py
@@ -5,18 +5,24 @@
 
 def main():
   # use the parse() function to load and parse an XML file
-
+  doc = xml.dom.minidom.parse("samplexml.xml")
   
   # print out the document node and the name of the first child tag
-
+  print(doc.nodeName)
+  print(doc.firstChild.tagName)
   
   # get a list of XML tags from the document and print each one
-
+  skills = doc.getElementsByTagName("skill")
+  print("%d skills: " % skills.length)
+  for skill in skills:
+    print(skill.getAttribute("name"))
     
   # create a new XML tag and add it into the document
-
+  newSkill = doc.createElement("skill")
+  newSkill.setAttribute("name", "Jquery")
+  doc.firstChild.appendChild(newSkill)
   
 
 if __name__ == "__main__":
-    main()
+  main()
 
